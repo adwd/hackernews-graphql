55b8a4ee40afb735f3c95db6ab302715240c5940
adwd <ecdt.mn@gmail.com>
2019-09-11 23:53:11 +0900

    add hacker news theme

--- ionic-app/src/App.tsx
+++ ionic-app/src/App.tsx
@@ -23,6 +23,8 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './theme/hacker-news.css';
+
 const App: React.FunctionComponent = () => (
   <IonApp>
     <IonReactRouter>
--- ionic-app/src/pages/Home.tsx
+++ ionic-app/src/pages/Home.tsx
@@ -1,23 +1,83 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButton,
+} from '@ionic/react';
 import React from 'react';
 
-const Home: React.FunctionComponent = () => {
+const Home = () => {
+  const [count, setCount] = React.useState(0);
   return (
     <>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+        <IonToolbar color="hack">
+          <IonTitle>Hacker News</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>Card Title</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Keep close to Natures heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit
+            clean.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon name="pin" slot="start" />
+            <IonLabel>{count}</IonLabel>
+            <IonButton
+              fill="outline"
+              slot="end"
+              onClick={() => setCount(count + 1)}
+            >
+              Increment
+            </IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags, within an
+            ion-cardContent element.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem href="#" class="activated">
+            <IonIcon name="wifi" slot="start" />
+            <IonLabel>Card Link Item 1 .activated</IonLabel>
+          </IonItem>
+
+          <IonItem href="#">
+            <IonIcon name="wine" slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+
+          <IonItem class="activated">
+            <IonIcon name="warning" slot="start" />
+            <IonLabel>Card Button Item 1 .activated</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon name="walk" slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
       </IonContent>
     </>
   );
