718f4ace24d800879ed4d692f3c99ebf2b78c397
adwd <ecdt.mn@gmail.com>
2019-09-12 00:38:01 +0900

    connect to server

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,9 @@ go 1.13
 require (
 	github.com/99designs/gqlgen v0.9.3
 	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/go-chi/chi v3.3.2+incompatible
+	github.com/gorilla/websocket v1.2.0
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser v1.1.2
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 )
--- server/server/server.go
+++ server/server/server.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/adwd/hackernews-graphql/server"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -17,9 +20,30 @@ func main() {
 		port = defaultPort
 	}
 
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(server.NewExecutableSchema(server.Config{Resolvers: &server.Resolver{}})))
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	router.Handle("/query",
+		handler.GraphQL(
+			server.NewExecutableSchema(server.Config{Resolvers: &server.Resolver{}}),
+			handler.WebsocketUpgrader(upgrader),
+		),
+	)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
