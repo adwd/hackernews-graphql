e373ac5de4df8bae6db91ec48b224b6365ed50f6
Masahiro Nishida <adwd@users.noreply.github.com>
2019-10-15 20:00:50 +0900

    add capacitor ios/android

--- ionic-app/src/graphql/apollo.ts
+++ ionic-app/src/graphql/apollo.ts
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const uri =
   process.env.NODE_ENV === 'production'
-    ? '/query'
+    ? 'https://app-qgegpghpna-an.a.run.app/query'
     : 'http://localhost:8080/query';
 
 export const client = new ApolloClient({
