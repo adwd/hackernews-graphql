f297cead255b9fd4e92779f0de16485c94c47107
Masahiro Nishida <adwd@users.noreply.github.com>
2019-10-16 19:05:19 +0900

    Add cordova plugin (#30)
    * add capacitor ios/android
    
    * fix package-lock.json
    
    * use cordova-plugin-safariviewcontroller

--- ionic-app/src/components/Story.tsx
+++ ionic-app/src/components/Story.tsx
@@ -58,7 +58,7 @@ export const Story = ({ story }: { story: StoryFragment }) => {
 function cardURL(story: StoryFragment) {
   // href attribute doesn't work correctly due to bug
   // https://github.com/ionic-team/ionic/issues/19241
-  return story.url ? { onClick: () => window.open(story.url!, '_blank') } : {};
+  return story.url ? { onClick: () => openUrl(story.url!, false) } : {};
 }
 
 function getHost(url: string | null) {
@@ -80,3 +80,56 @@ function ago(time: number): string {
   const diff = differenceInHours(Date.now(), d);
   return `${diff} hours ago`;
 }
+
+const isIOS = (function() {
+  try {
+    console.log(SafariViewController);
+    return true;
+  } catch (e) {
+    return false;
+  }
+})();
+
+// https://github.com/EddyVerbruggen/cordova-plugin-safariviewcontroller#4-usage
+function openUrl(url: string, readerMode: any) {
+  if (isIOS) {
+    SafariViewController.isAvailable(function(available: boolean) {
+      if (available) {
+        SafariViewController.show(
+          {
+            url: url,
+            hidden: false, // default false. You can use this to load cookies etc in the background (see issue #1 for details).
+            animated: false, // default true, note that 'hide' will reuse this preference (the 'Done' button will always animate though)
+            transition: 'curl', // (this only works in iOS 9.1/9.2 and lower) unless animated is false you can choose from: curl, flip, fade, slide (default)
+            enterReaderModeIfAvailable: readerMode, // default false
+            tintColor: '#ff6600', // default is ios blue
+            barColor: '#ff6600', // on iOS 10+ you can change the background color as well
+            controlTintColor: '#ffffff', // on iOS 10+ you can override the default tintColor
+          },
+          // this success handler will be invoked for the lifecycle events 'opened', 'loaded' and 'closed'
+          function(result: any) {
+            if (result.event === 'opened') {
+              console.log('opened');
+            } else if (result.event === 'loaded') {
+              console.log('loaded');
+            } else if (result.event === 'closed') {
+              console.log('closed');
+            }
+          },
+          function(msg: string) {
+            console.log('KO: ' + msg);
+          },
+        );
+      } else {
+        // potentially powered by InAppBrowser because that (currently) clobbers window.open
+        window.open(url, '_blank', 'location=yes');
+      }
+    });
+  } else {
+    window.open(url, '_blank');
+  }
+}
+
+// function dismissSafari() {
+//   SafariViewController.hide();
+// }
---
+++ ionic-app/src/index.env.d.ts
@@ -0,0 +1,1 @@
+declare var SafariViewController: any;
